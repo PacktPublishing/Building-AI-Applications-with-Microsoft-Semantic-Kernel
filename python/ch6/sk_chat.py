df047a62619cd52594cbf0778fc3ea38c0bbfdb8
Lucas Meyer <lucas.meyer@microsoft.com>
2024-05-01 18:06:28 -0700

    Updates to Python memory 0.96b

--- python/ch6/sk_chat.py
+++ python/ch6/sk_chat.py
@@ -1,84 +1,96 @@
 import asyncio
-import os
-from dotenv import load_dotenv
 import semantic_kernel as sk
-from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion, OpenAIChatPromptExecutionSettings
+from semantic_kernel.functions import KernelArguments, KernelFunction
+from semantic_kernel.prompt_template import PromptTemplateConfig, InputVariable
 from semantic_kernel.core_plugins import ConversationSummaryPlugin
-from semantic_kernel.functions import KernelArguments
-
-def create_kernel() -> sk.Kernel:
-    kernel = sk.Kernel()
-    api_key = os.getenv("OPENAI_API_KEY")
-    gpt = OpenAIChatCompletion("gpt-4-turbo-preview", api_key, None, "gpt4")
-    kernel.add_service(gpt)
-    
-    kernel.add_plugin(ConversationSummaryPlugin(kernel, None, "history"), "ConversationSummaryPlugin")
-    return kernel
-
-async def chat(kernel:sk.Kernel, context: KernelArguments) -> bool:   
-    user_input = input("User:> ")
-    context["userInput"] = user_input
-    if user_input == "exit":
-        print("\n\nExiting chat...")
-        return False
-
-    prompt = """  
-    Chat history:
-    {{$history}}
-
-    User: {{$userInput}}
-    Assistant: 
-    """
-
-    execution_settings = kernel.get_service("gpt4").instantiate_prompt_execution_settings(service_id="gpt4")
-    execution_settings.max_tokens = 4000
-    execution_settings.temperature = 0.8
-
-    f = kernel.add_function ("chat", function_name="chat", prompt=prompt, prompt_execution_settings=execution_settings)
-    answer = await kernel.invoke(f, arguments=context)
-    context["history"] += f"\nUser:> {user_input}\nAssistant:> {answer}\n"
-    print(f"Assistant:> {answer}")
-    return True
-
-async def chat_with_summarized_memory(kernel: sk.Kernel, context: KernelArguments) -> bool:   
-    user_input = input("User:> ")
-
-    context["userInput"] = user_input
-    if user_input == "exit":
-        print("\n\nExiting chat...")
-        return False
-
-    prompt = """  
-    Chat history:
-    {{ConversationSummaryPlugin.SummarizeConversation $history}}
-
-    User: {{$userInput}}
-    Assistant: 
-    """
-
-    execution_settings = kernel.get_service("gpt4").instantiate_prompt_execution_settings(service_id="gpt4")
-    execution_settings.max_tokens = 4000
-    execution_settings.temperature = 0.8
-    f = kernel.add_function("chat", function_name="chat", prompt=prompt, prompt_execution_settings=execution_settings)
-
-    answer = await kernel.invoke(f, arguments=context)
-    context["history"] += f"\nUser:> {user_input}\nAssistant:> {answer}\n"
-    print(f"Assistant:> {answer}")
-    return True
-
+from semantic_kernel.contents.chat_history import ChatHistory
+from semantic_kernel.utils.settings import openai_settings_from_dot_env
 
 async def main():
-    load_dotenv()
     kernel = create_kernel()
+    history = ChatHistory()
 
-    context = KernelArguments()
-    context["history"] = ""
+    chat_function = await create_chat_function(kernel)
 
-    print("Begin chatting (type 'exit' to exit):\n")
-    chatting = True
-    while chatting:
-        chatting = await chat_with_summarized_memory(kernel, context)
-        chatting = await chat(kernel, context)
+    while True:
+        try:
+            request = input("User:> ")
+        except KeyboardInterrupt:
+            print("\n\nExiting chat...")
+            return False
+        except EOFError:
+            print("\n\nExiting chat...")
+            return False
+
+        if request == "exit":
+            print("\n\nExiting chat...")
+            return False
+
+        result = await kernel.invoke(
+            chat_function,
+            request=request,
+            history=history,
+        )
+
+        # Add the request to the history
+        history.add_user_message(request)
+        history.add_assistant_message(str(result))
+
+        print(f"Assistant:> {result}")
+
+def create_kernel() -> sk.Kernel:
+    api_key, org_id =  openai_settings_from_dot_env()
+    kernel = sk.Kernel()
+    gpt = OpenAIChatCompletion(ai_model_id="gpt-4-turbo-preview", api_key=api_key, org_id=org_id, service_id="gpt4")
+    kernel.add_service(gpt)
+
+    # The following execution settings are used for the ConversationSummaryPlugin
+    execution_settings = OpenAIChatPromptExecutionSettings(
+        service_id="gpt4", max_tokens=ConversationSummaryPlugin._max_tokens, temperature=0.1, top_p=0.5)
+    
+    prompt_template_config = PromptTemplateConfig(
+        template=ConversationSummaryPlugin._summarize_conversation_prompt_template,
+        description="Given a section of a conversation transcript, summarize it",
+        execution_settings=execution_settings,
+    )    
+
+    # Import the ConversationSummaryPlugin
+    kernel.add_plugin(
+        ConversationSummaryPlugin(kernel=kernel, prompt_template_config=prompt_template_config),
+        plugin_name="ConversationSummaryPlugin",
+    )
+
+    return kernel
+
+
+async def create_chat_function(kernel: sk.Kernel) -> KernelFunction:   
+    # Create the prompt with the ConversationSummaryPlugin
+    prompt = """{{ConversationSummaryPlugin.SummarizeConversation $history}}
+    User: {{$request}}
+    Assistant:  """
+
+    # These execution settings are tied to the chat function, created below.
+    execution_settings = kernel.get_service("gpt4").instantiate_prompt_execution_settings(service_id="gpt4")
+    chat_prompt_template_config = PromptTemplateConfig(
+        template=prompt,
+        description="Chat with the assistant",
+        execution_settings=execution_settings,
+        input_variables=[
+            InputVariable(name="request", description="The user input", is_required=True),
+            InputVariable(name="history", description="The history of the conversation", is_required=True),
+        ],
+    )
+
+    # Create the function
+    chat_function = kernel.add_function(
+        prompt=prompt,
+        plugin_name="Summarize_Conversation",
+        function_name="Chat",
+        description="Chat with the assistant",
+        prompt_template_config=chat_prompt_template_config,)
+    
+    return chat_function
 
 
 if __name__ == "__main__":
--- python/ch6/sk_mem.py
+++ python/ch6/sk_mem.py
@@ -1,60 +1,61 @@
 import asyncio
-import os
-from dotenv import load_dotenv
 import semantic_kernel as sk
-from semantic_kernel.connectors.ai.open_ai import OpenAITextEmbedding, OpenAIChatCompletion
+from semantic_kernel.connectors.ai.open_ai import OpenAITextEmbedding, OpenAIChatCompletion, OpenAIChatPromptExecutionSettings
 from semantic_kernel.memory.volatile_memory_store import VolatileMemoryStore
 from semantic_kernel.memory.semantic_text_memory import SemanticTextMemory
 from semantic_kernel.core_plugins.text_memory_plugin import TextMemoryPlugin
-from semantic_kernel.functions import KernelArguments
+from semantic_kernel.functions import KernelArguments, KernelFunction
+from semantic_kernel.prompt_template import PromptTemplateConfig
+from semantic_kernel.utils.settings import openai_settings_from_dot_env
+
 
 async def add_to_memory(memory: SemanticTextMemory, id: str, text: str):
     await memory.save_information(collection="generic", id=id, text=text)
 
-def create_kernel() -> sk.Kernel:
+def create_kernel() -> tuple[sk.Kernel, OpenAITextEmbedding]:
+    api_key, org_id =  openai_settings_from_dot_env()
     kernel = sk.Kernel()
-    api_key = os.getenv("OPENAI_API_KEY")
-    emb = OpenAITextEmbedding(ai_model_id="text-embedding-3-small", api_key=api_key, org_id="", service_id="emb3")
-    gpt = OpenAIChatCompletion(ai_model_id="gpt-4-turbo-preview", api_key=api_key, org_id="", service_id="gpt4")
-    kernel.add_service(gpt)
+    gpt = OpenAIChatCompletion(ai_model_id="gpt-4-turbo-preview", api_key=api_key, org_id=org_id, service_id="gpt4")
+    emb = OpenAITextEmbedding(ai_model_id="text-embedding-ada-002", api_key=api_key, org_id=org_id, service_id="emb")
     kernel.add_service(emb)
+    kernel.add_service(gpt)
+    return kernel, emb
 
-    memory = SemanticTextMemory(storage=VolatileMemoryStore(), embeddings_generator=emb)
-    kernel.add_plugin(TextMemoryPlugin(memory), "TextMemoryPlugin")
-    return kernel, memory
-
-async def tour(kernel: sk.Kernel):
-    prompt = """  
+async def tour(kernel: sk.Kernel) -> KernelFunction:
+    prompt = """
     Information about me, from previous conversations:
     - {{$city}} {{recall $city}}
     - {{$activity}} {{recall $activity}}
-
-    Generate a personalized tour of activities for me to do when I have a free day in my favorite city. I just want to do my favorite activity.
-    """
+    """.strip()
 
     execution_settings = kernel.get_service("gpt4").instantiate_prompt_execution_settings(service_id="gpt4")
     execution_settings.max_tokens = 4000
     execution_settings.temperature = 0.8
 
-    context = KernelArguments()
+    prompt_template_config = PromptTemplateConfig(template=prompt, execution_settings=execution_settings)    
+    chat_func = kernel.add_function(
+        function_name="chat_with_memory",
+        plugin_name="TextMemoryPlugin",
+        prompt_template_config=prompt_template_config,
+    )
 
-    f = kernel.add_function ("chat", function_name="chat", prompt=prompt, prompt_execution_settings=execution_settings)
-    context["city"] = "What's my favorite city?"
-    context["activity"] = "What is my favorite activity?"
-
-    return f, context
+    return chat_func
 
 
 async def main():
-    load_dotenv()
-    kernel, memory = create_kernel()
+    kernel, emb = create_kernel()
+    memory = SemanticTextMemory(storage=VolatileMemoryStore(), embeddings_generator=emb)
+    kernel.add_plugin(TextMemoryPlugin(memory), "TextMemoryPlugin")
 
-    await add_to_memory(memory, "1", "My favorite city is Paris")
-    await add_to_memory(memory, "2", "My favorite activitity is visiting museums")
+    await add_to_memory(memory, id="info1", text="My favorite city is Paris")   
+    await add_to_memory(memory, id="info2", text="My favorite activity is visiting museums")
 
-    f, context = await tour(kernel)
+    f = await tour(kernel)
 
-    answer = await kernel.invoke(f, arguments=context)
+    args = KernelArguments()
+    args["city"] = "My favorite city is Paris"
+    args["activity"] = "My favorite activity is visiting museums" 
+    answer = await kernel.invoke(f, arguments=args)
     print(answer)
 
 if __name__ == "__main__":
